f64858d0010011525051b25eba741f9df08a94db
vsilbermann <vsi@even-on-sunday.com>
2018-08-07 16:54:42 +0200

    change development mode detection (used server IP's are not reliable)

--- my_symfony_project/web/app_dev.php
+++ my_symfony_project/web/app_dev.php
@@ -8,13 +8,15 @@ use Symfony\Component\HttpFoundation\Request;
 // for more information
 //umask(0000);
 
-//echo  $_SERVER['REMOTE_ADDR']; die();
+$host = @reset(explode(':', $_SERVER['HTTP_HOST'] ?? ''));
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1','172.23.0.1', '172.24.0.1', '::1'), true) || PHP_SAPI === 'cli-server')
+    || !(in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'), true)
+        || $host === 'localhost'
+        || PHP_SAPI === 'cli-server')
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
