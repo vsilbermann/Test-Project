e2e81344ef7bda4313b14c1a9740e985dc4fbe0f
ewesseloh <eugen.wesseloh@even-on-sunday.com>
2018-08-10 09:36:30 +0200

    composer: replace doctrine with propel ORM

--- my_symfony_project/app/AppKernel.php
+++ my_symfony_project/app/AppKernel.php
@@ -13,7 +13,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Propel\Bundle\PropelBundle\PropelBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
--- my_symfony_project/app/autoload.php
+++ my_symfony_project/app/autoload.php
@@ -1,15 +1,13 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 
-$loader->add('Propel' , __DIR__.'/../vendor/bundles');
-
-
-$loader->add('Phing' , __DIR__.'/../vendor/phing/classes/phing');
+$loader->add('Propel', __DIR__.'/../vendor/bundles');
+$loader->add('Phing', __DIR__.'/../vendor/phing/classes/phing');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
